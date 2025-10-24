d46e89a00ec3c22e215e6e20cd729dc74d5fe554
RandomNerd01 <suiron8@gmail.com>
2025-10-24 15:25:33 +0800

    Fix List Command and Implement Tests

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,46 +1,78 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
 /**
- * Lists all contacts in the address book to the user.
- * Shows all contacts with index numbers in the format:
- * "[index]. NAME | PHONE | [EMAIL] | [COMPANY] | [TAGS]".
- * If the list is empty, outputs "No contacts stored".
+ * Lists all persons in the address book or persons with a specific tag.
  */
 public class ListCommand extends Command {
 
-    public static final String MESSAGE_SUCCESS = "Listed all contacts!";
     public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_USAGE = "Listing all contacts: list\n\n"
+            + "Shows all contacts in your database. You can also filter by tags.\n\n"
+            + "Format:\n\n"
+            + "list - Shows all contacts\n\n"
+            + "list t/TAG - Shows contacts with specific tag (learn about tags in the add command section)\n\n"
+            + "Examples:\n\n"
+            + "list\n"
+            + "list t/priority\n"
+            + "list t/designer";
+
+    public static final String MESSAGE_NO_CONTACTS_STORED = "No contacts stored";
+
+    private final String tagName; // null => show all
+
+    public ListCommand() {
+        this.tagName = null;
+    }
+
+    /**
+     * List only persons having this tag name (case-insensitive).
+     */
+    public ListCommand(String tagName) {
+        this.tagName = tagName;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
 
-        List<Person> contacts = model.getFilteredPersonList();
-        String feedbackToUser;
-
-        if (contacts.isEmpty()) {
-            feedbackToUser = "No contacts stored";
+        if (tagName == null) {
+            // show all persons
+            model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
         } else {
-            // Build the formatted string
-            feedbackToUser = contacts.stream()
-                    .map(person -> formatPerson(person, contacts.indexOf(person) + 1))
-                    .collect(Collectors.joining("\n"));
+            final String tagLower = tagName.toLowerCase();
+            model.updateFilteredPersonList(person -> person.getTags().stream()
+                    .map(t -> t.tagName.toLowerCase())
+                    .anyMatch(tn -> tn.equals(tagLower)));
         }
 
-        return new CommandResult(feedbackToUser);
+        List<Person> filtered = model.getFilteredPersonList();
+
+        if (filtered.isEmpty()) {
+            return new CommandResult(MESSAGE_NO_CONTACTS_STORED);
+        }
+
+        String result = filtered.stream()
+                .map(p -> formatPersonForList(p, model))
+                .collect(Collectors.joining("\n"));
+
+        // keep same behavior as previous ListCommand: return the formatted string
+        return new CommandResult(result);
     }
 
-    private String formatPerson(Person person, int index) {
+    /**
+     * Matches the formatting expected by the tests:
+     * "%d. %s | %s | [%s] | [%s] | %s"
+     */
+    private static String formatPersonForList(Person person, Model model) {
+        int index = model.getFilteredPersonList().indexOf(person) + 1;
         return String.format("%d. %s | %s | [%s] | [%s] | %s",
                 index,
                 person.getName(),
@@ -48,5 +80,26 @@ public class ListCommand extends Command {
                 person.getEmail(),
                 person.getCompany(),
                 person.getTags());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        } else if (!(other instanceof ListCommand)) {
+            return false;
+        } else {
+            ListCommand otherCmd = (ListCommand) other;
+            if (this.tagName == null) {
+                return otherCmd.tagName == null;
+            } else {
+                return this.tagName.equalsIgnoreCase(otherCmd.tagName);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ListCommand.class.getCanonicalName() + "{tag=" + tagName + "}";
     }
 }
