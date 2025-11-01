2ddc81a3df9e17882163a5774d396a8c801cdf23
jgkiran2003 <jgkiran2003@gmail.com>
2025-11-01 14:22:41 +0800

    Revert "Check duplicate by case insensitive name"
    This reverts commit 5e170046e0c26281f43186d82b251eb5f91b8d5e.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -57,7 +57,7 @@ public class Name {
         }
 
         Name otherName = (Name) other;
-        return fullName.equalsIgnoreCase(otherName.fullName);
+        return fullName.equals(otherName.fullName);
     }
 
     @Override
